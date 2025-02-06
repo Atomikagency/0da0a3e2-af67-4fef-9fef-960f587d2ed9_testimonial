0ebdca16d2009b295be7b69cc25d0dc291cf54b2
Kevin Janiky <kevin.janiky@gmail.com>
2025-02-06 11:20:08 +0100

    base PHP

---
+++ src/testimonial.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: Testimonial
+ * Description: Add the capability to request feedback from your customers and display it on your website.
+ * Version: 1.0.0
+ * Author: AtomikAgency
+ * Author URI: https://atomikagency.fr/
+ */
+
+define('TESTIMONIAL_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('TESTIMONIAL_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+if (!defined('ABSPATH')) {
+    exit; // Prevent direct access
+}
+
+require_once TESTIMONIAL_PLUGIN_DIR . 'update-checker.php';
